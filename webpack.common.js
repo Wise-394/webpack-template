948d717ff2eaf423b4c0938559d814dd63c0466d
wise <114738222+Wise-394@users.noreply.github.com>
2025-09-08 20:07:00 +0800

    added webpack.common.js

---
+++ webpack.common.js
@@ -0,0 +1,32 @@
+// webpack.common.js
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+
+  output: {
+    filename: "bundle.js", // Will be overridden in prod with hashing
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/template.html",
+    }),
+  ],
+};
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,48 +1,37 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { merge } = require("webpack-merge");
+const common = require("./webpack.common.js");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 
-module.exports = {
+module.exports = merge(common, {
   mode: "production",
-  entry: "./src/index.js",
+
   output: {
-    filename: "[name].[contenthash].js", // Optimized for caching
-    path: path.resolve(__dirname, "dist"),
-    clean: true,
+    filename: "[name].[contenthash].js",
   },
-  // devtool is for development only, remove it for production
-  
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./src/template.html",
-    }),
-    new MiniCssExtractPlugin({ // Extracts CSS to a separate file
-      filename: "[name].[contenthash].css",
-    }),
-  ],
+
   module: {
     rules: [
       {
         test: /\.css$/i,
-        use: [MiniCssExtractPlugin.loader, "css-loader"], // Use MiniCssExtractPlugin for production
-      },
-      {
-        test: /\.html$/i,
-        loader: "html-loader",
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: "asset/resource",
+        use: [MiniCssExtractPlugin.loader, "css-loader"], // Extract CSS into files
       },
     ],
   },
+
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "[name].[contenthash].css",
+    }),
+  ],
+
   optimization: {
-    minimizer: [ // Minify CSS for smaller file sizes
+    minimizer: [
+      `...`, // keep default minimizers (like Terser for JS)
       new CssMinimizerPlugin(),
     ],
-    splitChunks: { // Separate code for better caching
-      chunks: 'all',
+    splitChunks: {
+      chunks: "all",
     },
   },
-};
+});
